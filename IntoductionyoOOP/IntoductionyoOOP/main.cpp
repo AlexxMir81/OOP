9f9545f335f2240d20a52262e9bca4e51299b6ba
Miro_up10@itstepru.onmicrosoft.com <Miro_up10@itstepru.onmicrosoft.com>
2021-10-02 13:44:27 +0300

    CodePage UTF8

--- IntoductionyoOOP/IntoductionyoOOP/main.cpp
+++ IntoductionyoOOP/IntoductionyoOOP/main.cpp
@@ -1,10 +1,10 @@
-#include<iostream>
+ï»¿#include<iostream>
 using namespace std;
 
 #define tab "\t"
 
 
-//ñîçäàíèå ñòðóêòóðû
+//ÑÐ¾Ð·Ð´Ð°Ð½Ð¸Ðµ ÑÑ‚Ñ€ÑƒÐºÑ‚ÑƒÑ€Ñ‹
 class Point
 {
 	double x;
@@ -26,6 +26,25 @@ public:
 	{
 		this->y = y;
 	}
+
+	//        Constructors:
+	Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	Point(double x=0, double y=0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//        Methods:
 	double distance(Point other)
 	{
 		double x_distance = this->x - other.x;
@@ -33,18 +52,23 @@ public:
 		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
 		return distance;
 	}
+
+	void print() const
+	{
+		cout << "X=" << x << "\t" << "Y=" << y << endl;
+	}
 };
 
 
 double distance(Point A, Point B);
 //#define STRUCT_POINT
-
+//#define distance
 void main()
 {
 	setlocale(LC_ALL, "Russian");
 #ifdef STRUCT_POINT
 
-	Point A;  //ñîçäàíèå îáúåêòà À ñòðóêòóðû Point/ ñîçäàíèå ýêçåìëÿðà ñòðóêòóðû Point
+	Point A;  //ÑÐ¾Ð·Ð´Ð°Ð½Ð¸Ðµ Ð¾Ð±ÑŠÐµÐºÑ‚Ð° Ð ÑÑ‚Ñ€ÑƒÐºÑ‚ÑƒÑ€Ñ‹ Point/ ÑÐ¾Ð·Ð´Ð°Ð½Ð¸Ðµ ÑÐºÐ·ÐµÐ¼Ð»ÑÑ€Ð° ÑÑ‚Ñ€ÑƒÐºÑ‚ÑƒÑ€Ñ‹ Point
 	A.x = 2;
 	A.y = 3;
 	cout << A.x << tab << A.y << endl;
@@ -53,6 +77,8 @@ void main()
 	cout << pA->x << tab << pA->y << endl;
 	cout << (*pA).x << tab << (*pA).y << endl;
 #endif
+
+#ifdef distance
 
 	Point A;
 	A.set_x(2);
@@ -68,6 +94,12 @@ void main()
 	cout << A.distance(B) << endl;
 	cout << distance(A, B) << endl;
 	cout << distance(B, A) << endl;
+#endif 
+
+	Point A;   //Default constuctor
+	A.print();
+	Point B(2,3);
+	B.print();
 }
 
 double distance(Point A, Point B)
